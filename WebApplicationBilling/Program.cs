069c9b5f4e0a7bc1b1e669b15aee365ef8d5140a
Johanbeltranramirez <johanframirez13@gmail.com>
2023-12-06 17:56:31 -0500

    dev(git): add custumers.js

--- WebApplicationBilling/Program.cs
+++ WebApplicationBilling/Program.cs
@@ -1,14 +1,25 @@
+using WebApplicationBilling.Repository;
+using WebApplicationBilling.Repository.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+//Habilitar el cliente Http
+builder.Services.AddHttpClient();
+
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -18,8 +29,17 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//Damos Soporte Cors
+app.UseCors(c => c
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
